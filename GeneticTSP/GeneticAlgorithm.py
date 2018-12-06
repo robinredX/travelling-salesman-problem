9598d7f61e6c4c57f01255052032eb6359f84d64
pauldoust <m.pauldoust@gmail.com>
2018-12-06 12:52:59 +0100

    change output way

--- GeneticTSP/GeneticAlgorithm.py
+++ GeneticTSP/GeneticAlgorithm.py
@@ -31,7 +31,7 @@ class Individual:
     def __init__(self, genes =[]):
         self.chromosomeGenes = genes
         self.fitness = 0
-        print("const, len: ",len(self.chromosomeGenes))
+        # print("const, len: ",len(self.chromosomeGenes))
         if len(self.chromosomeGenes) == 0  :
             for i in range (0, CHROMOSOME_LENGTH):
                 self.chromosomeGenes.append(None)
@@ -40,10 +40,11 @@ class Individual:
         return len(self.chromosomeGenes)
 
     def __repr__(self):
-        geneString=""
-        for i in range(0, len(self.chromosomeGenes)):
-            geneString += str(self.chromosomeGenes[i]) + "->"
-        return geneString
+        # geneString=""
+        # for i in range(0, len(self.chromosomeGenes)):
+        #     geneString += str(self.chromosomeGenes[i]) + "->"
+        self.chromosomeGenes.append(self.chromosomeGenes[0])
+        return str(self.chromosomeGenes)
 
     def __getitem__(self, index):
       return self.chromosomeGenes[index]
@@ -69,7 +70,6 @@ class Individual:
                 firstGene = self.chromosomeGenes[i]
                 seconGene = self.chromosomeGenes[i+1]
                 initialFitness += DISTANCES[firstGene.getGeneId()] [seconGene.getGeneId()]
-        # print 
         initialFitness += DISTANCES[self.chromosomeGenes[-1].getGeneId()] [self.chromosomeGenes[0].getGeneId()]
 
         return initialFitness
@@ -82,7 +82,7 @@ class Population:
         if len(self.individuals) == POPULATION_SIZE:
             return
         for i in range(0, populationSize):
-            print(i)
+            # print(i)
             self.individuals.append(None)
 
 
@@ -123,20 +123,20 @@ class Universe:
         midIndex = int(random.random() * father.getChromosomeLength())
         if mid is not None:
             midIndex = mid
-        print("mid: ", midIndex)
-        print("father: ", father)
-        print("mother: ", mother)
+        # print("mid: ", midIndex)
+        # print("father: ", father)
+        # print("mother: ", mother)
 
         addedGenes = []
-        print("initial: ", child)
+        # print("initial: ", child)
         for i in range(0, midIndex):
             # print("from Father: ", i)
             child.setGeneAtIndex(father.getGeneAtIndex(i), i )
             addedGenes.append(father.getGeneAtIndex(i).getGeneId())
-        print("child from father: ", child)
+        # print("child from father: ", child)
 
         if len(addedGenes) == CHROMOSOME_LENGTH:
-            print("final child all from father ", child)
+            # print("final child all from father ", child)
             return child
 
         index = 0
@@ -149,7 +149,7 @@ class Universe:
 
 
         child = self.mutate(child)
-        print("final child: ", child)
+        # print("final child: ", child)
         return child
 
     def mutate(self, individual):
@@ -196,8 +196,8 @@ class Universe:
             motherToMate = self.selection(currentPoplulation, elitedIndividuals)
 
             # print("current Pop: ", currentPoplulation.individuals)
-            print("fatherToMate: ", fatherToMate)
-            print("motherToMate: ", motherToMate)
+            # print("fatherToMate: ", fatherToMate)
+            # print("motherToMate: ", motherToMate)
             child = self.crossover(fatherToMate, motherToMate)
             # print("child: ", child)
             nexGeneration.setIndividualAtIndex(child, nextGenerationSize)
@@ -230,7 +230,7 @@ class Genetic:
         for i in range (0, self.matrix.shape[0]):
             city = Gene(str(i + 1), i)
             cities.append(city)
-        print(cities)
+        # print(cities)
         tour1 = Individual(cities)
         initialPopulation = [tour1]
         for i in range(1, POPULATION_SIZE):
@@ -238,12 +238,12 @@ class Genetic:
             random.shuffle(newTour)
             initialPopulation.append(newTour)
 
-        print(initialPopulation)
+        # print(initialPopulation)
 
         pop = Population(initialPopulation, POPULATION_SIZE)
         univ = Universe()
         for i in range(0,GENERATION_NUM):
-            print("Pop: ,", pop.individuals)
+            # print("Pop: ,", pop.individuals)
             pop = univ.breed(pop)
 
         fittest = pop.getFittest()
@@ -251,4 +251,28 @@ class Genetic:
         #print("Fittest is : ", fittest, " , With Fitness: ", fittest.getFitness())
 
 
-#main()
+DISTANCES =np.array ([ 
+
+                [  0, 633, 257,  91, 412, 150,  80, 134, 259, 505, 353, 324,  70, 211, 268, 246, 121],
+                [633,   0, 390, 661, 227, 488, 572, 530, 555, 289, 282, 638, 567, 466, 420, 745, 518],
+                [257, 390,   0, 228, 169, 112, 196, 154, 372, 262, 110, 437, 191,  74,  53, 472, 142,],
+                [ 91, 661, 228,   0, 383, 120,  77, 105, 175, 476, 324, 240,  27, 182, 239, 237,  84],
+                [412, 227, 169, 383,   0, 267, 351, 309, 338, 196,  61, 421, 346, 243, 199, 528, 297],
+                [150, 488, 112, 120, 267,   0,  63,  34, 264, 360, 208, 329,  83, 105, 123, 364,  35],
+                [ 80, 572, 196,  77, 351,  63,   0,  29, 232, 444, 292, 297,  47, 150, 207, 332,  29],
+                [134, 530, 154, 105, 309,  34,  29,   0, 249, 402, 250, 314,  68, 108, 165, 349,  36],
+                [259, 555, 372, 175, 338, 264, 232, 249,   0, 495, 352,  95, 189, 326, 383, 202, 236],
+                [505, 289, 262, 476, 196, 360, 444, 402, 495,   0, 154, 578, 439, 336, 240, 685, 390],
+                [353, 282, 110, 324,  61, 208, 292, 250, 352, 154,   0, 435, 287, 184, 140, 542, 238],
+                [324, 638, 437, 240, 421, 329, 297, 314,  95, 578, 435,   0, 254, 391, 448, 157, 301],
+                [ 70, 567, 191,  27, 346,  83,  47,  68, 189, 439, 287, 254,   0, 145, 202, 289,  55],
+                [211, 466,  74, 182, 243, 105, 150, 108, 326, 336, 184, 391, 145,   0,  57, 426,  96],
+                [268, 420,  53, 239, 199, 123, 207, 165, 383, 240, 140, 448, 202,  57,   0, 483, 153],
+                [246, 745, 472, 237, 528, 364, 332, 349, 202, 685, 542, 157, 289, 426, 483,   0, 336],
+                [121, 518, 142, 84, 297,  35,  29,  36, 236, 390, 238, 301,  55,  96, 153, 336 ,  0 ]
+     
+                ])
+
+ga = Genetic(DISTANCES)
+
+print(ga.main())
