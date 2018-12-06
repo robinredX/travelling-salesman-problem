de18fa1d9fafb957c683d6875a0b4af666fbebca
pauldoust <m.pauldoust@gmail.com>
2018-12-06 12:20:47 +0100

    Optimizing the performance by extra pruning (more salable)

--- BnBAddingRemovingEdges/BnB.py
+++ BnBAddingRemovingEdges/BnB.py
@@ -5,6 +5,7 @@ import time
 
 INFINITY = np.inf
 
+
 # DISTANCES = [ 
 #                [INFINITY, 3, 93, 13, 33, 9, 57],
 #                [4, INFINITY, 77, 42, 21, 16, 34],
@@ -13,14 +14,6 @@ INFINITY = np.inf
 #                [28, 46, 88, 33, INFINITY, 25, 57],
 #                [3, 88, 18, 46, 92, INFINITY, 7],
 #                [44, 26, 33, 27, 84, 39, INFINITY]
-#             ]
-
-# DISTANCES = [ 
-#                [INFINITY, 20, 30, 10, 11],
-#                [15, INFINITY, 16, 4, 2],
-#                [3, 5, INFINITY, 2, 4],
-#                [19, 6, 18, INFINITY, 3],
-#                [16, 4, 7, 16, INFINITY]
 #             ]
 
 class AddRemoveEdges:
@@ -93,14 +86,14 @@ class AddRemoveEdges:
         # print("maximumLowerBound for splitting: ", maximumLowerBound)
         return splittingCandidateRow, splittingCandidateCol
 
-    def includeEdge(self, matrix, i,j, lowerBound):
+    # def includeEdge(self, matrix, i,j, lowerBound):
 
-        matrix[j,i] = INFINITY
-        matrix[i,:] = np.full((1,matrix.shape[1]), np.nan)
-        matrix[:,j] = np.full((1,matrix.shape[0]), np.nan)
+    #     matrix[j,i] = INFINITY
+    #     matrix[i,:] = np.full((1,matrix.shape[1]), np.nan)
+    #     matrix[:,j] = np.full((1,matrix.shape[0]), np.nan)
 
-        matrix, lowerBound = self.reduceMatrix(matrix, lowerBound)
-        return matrix, lowerBound
+    #     matrix, lowerBound = self.reduceMatrix(matrix, lowerBound)
+    #     return matrix, lowerBound
 
 
     def excludeEdge(self, matrix, i, j,lowerBound):
@@ -122,6 +115,62 @@ class AddRemoveEdges:
         node_list.append("1")
         return node_list
 
+    def includeEdge(self, matrix, i,j, lowerBound, solution):
+
+        if (np.isnan (matrix[j,i])) == False:
+            matrix[j,i] = INFINITY
+        # matrix[j,i] = INFINITY
+        matrix[i,:] = np.full((1,matrix.shape[1]), np.nan)
+        matrix[:,j] = np.full((1,matrix.shape[0]), np.nan)
+
+
+
+        matrix, lowerBound = self.reduceMatrix(matrix, lowerBound)
+
+        sol = solution.copy()
+        edgeToPrint = str(i+1) + "," + str(j+1)
+        sol.append(edgeToPrint)
+        if len(sol) != matrix.shape[0] -1:
+            subpathes=[]
+            for edge in sol:
+                source = edge.split(",")[0]
+                target = edge.split(",")[1]
+                subpath=[]
+                subpath.append(edge)
+                noMore = True
+                newTraget = target
+                print("Sol: ", sol)
+                while noMore:      
+                    # print("Stuck Here", matrix.shape[0])
+                    # print("Stuck Here", sol)
+                    print("new target: ", newTraget)
+                    # matching = [s for s in sol if str('\'',newTraget+',') in s]
+                    matching = [s for s in sol if newTraget == s.split(",")[0]]
+                    # matching
+                    print("matching: ", matching)
+                    if len(matching) == 0:
+                        noMore = False
+                        break
+                    subpath.extend(matching)
+                    newTraget = matching[0]
+                    newTraget = newTraget.split(",")[1]
+                    if newTraget == target:
+                        noMore =False
+
+
+                lastEdge  = subpath[-1]
+                firstEdge = subpath[0]
+                lastNode = lastEdge.split(",")[1]
+                firstNode = firstEdge.split(",")[0]
+                lastNode = int(lastNode)
+                firstNode = int(firstNode)
+                lastNode = lastNode -1
+                firstNode = firstNode -1
+                if (np.isnan(matrix[lastNode, firstNode]) == False):
+                 matrix[lastNode, firstNode] = INFINITY
+
+
+        return matrix, lowerBound
 
 
     def isValidSolution (self, matrix, solution):
@@ -191,7 +240,7 @@ class AddRemoveEdges:
         print("split on: ", i+1, " ", j+1)
 
 
-        includeEdgeMat,IncludeLowerBound = self.includeEdge(np.copy(matrix), i,j, lowerBound)
+        includeEdgeMat,IncludeLowerBound = self.includeEdge(np.copy(matrix), i,j, lowerBound, solution)
         edgeToPrint = str(i+1) + "," + str(j+1)
         edge = str(i) + "," + str(j)
         # print("include(",edgeToPrint," )", "\nCost: ", IncludeLowerBound," \nMatrix: \n",includeEdgeMat )
@@ -223,3 +272,33 @@ class AddRemoveEdges:
         #print("Final Cost = ", upperBound)
 
 #main()
+
+
+''' Uncomment the above code to test  '''
+'''
+DISTANCES =  [ 
+
+                [  INFINITY, 633, 257,  91, 412, 150,  80, 134, 259, 505, 353, 324,  70, 211, 268, 246, 121],
+                [633,   INFINITY, 390, 661, 227, 488, 572, 530, 555, 289, 282, 638, 567, 466, 420, 745, 518],
+                [257, 390,   INFINITY, 228, 169, 112, 196, 154, 372, 262, 110, 437, 191,  74,  53, 472, 142,],
+                [ 91, 661, 228,   INFINITY, 383, 120,  77, 105, 175, 476, 324, 240,  27, 182, 239, 237,  84],
+                [412, 227, 169, 383,   INFINITY, 267, 351, 309, 338, 196,  61, 421, 346, 243, 199, 528, 297],
+                [150, 488, 112, 120, 267,   INFINITY,  63,  34, 264, 360, 208, 329,  83, 105, 123, 364,  35],
+                [ 80, 572, 196,  77, 351,  63,   INFINITY,  29, 232, 444, 292, 297,  47, 150, 207, 332,  29],
+                [134, 530, 154, 105, 309,  34,  29,   INFINITY, 249, 402, 250, 314,  68, 108, 165, 349,  36],
+                [259, 555, 372, 175, 338, 264, 232, 249,   INFINITY, 495, 352,  95, 189, 326, 383, 202, 236],
+                [505, 289, 262, 476, 196, 360, 444, 402, 495,   INFINITY, 154, 578, 439, 336, 240, 685, 390],
+                [353, 282, 110, 324,  61, 208, 292, 250, 352, 154,   INFINITY, 435, 287, 184, 140, 542, 238],
+                [324, 638, 437, 240, 421, 329, 297, 314,  95, 578, 435,   INFINITY, 254, 391, 448, 157, 301],
+                [ 70, 567, 191,  27, 346,  83,  47,  68, 189, 439, 287, 254,   INFINITY, 145, 202, 289,  55],
+                [211, 466,  74, 182, 243, 105, 150, 108, 326, 336, 184, 391, 145,   INFINITY,  57, 426,  96],
+                [268, 420,  53, 239, 199, 123, 207, 165, 383, 240, 140, 448, 202,  57,   INFINITY, 483, 153],
+                [246, 745, 472, 237, 528, 364, 332, 349, 202, 685, 542, 157, 289, 426, 483,   INFINITY, 336],
+                [121, 518, 142, 84, 297,  35,  29,  36, 236, 390, 238, 301,  55,  96, 153, 336 ,  INFINITY ]
+     
+                ]
+
+a = AddRemoveEdges(DISTANCES)
+print(a.main())
+'''
+
