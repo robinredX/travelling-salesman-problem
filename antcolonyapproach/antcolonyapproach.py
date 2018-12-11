d02dcf27373027f8df2a5a802d7aadcee66772f8
robinredX <robinkhatri@hotmail.com>
2018-12-11 10:59:09 +0100

    Update antcolonyapproach.py

--- antcolonyapproach/antcolonyapproach.py
+++ antcolonyapproach/antcolonyapproach.py
@@ -1,32 +1,45 @@
-import time
-import math
+### Robin Khatri
+### MLDM M1
+### Advanced algorithm TSP project
+
 import random
+import time
 
 class AntApproach:
-    def __init__(self, input, n=15, iteration=5000, a=1, b=1 , r=0.9, q=0.5, s=2):
-        self.input=input
-        self.n = n # number of ants
-        self.iteration = iteration
-        self.a = a # parameter alpha; See definition of parameters at https://en.wikipedia.org/wiki/Ant_colony_optimization_algorithms#Convergence
-        self.b = b # Parameter beta
-        self.r = r # pheromone evaporation coefficient 
-        self.q = q # it's a constant
-        self.s = s # Update strategy
+    def __init__(self, input, iterations=100, ants=10):
+        """
+        Input is a 2-D list which contains cost of travel
+        iterations define number of iterations. For most part 100 iterations work very well.
+        40 is the number of ants. Should be closer to the number of cities. We chose in [10,50]
+        """
+        self.dataset = input
+        self.iterations=iterations
+        self.ants = ants
         
-    def algo(self):
-        start_time = time.time() # Start clock
-        
-        dataset = self.input
-        for i in range(len(dataset)):
-            for j in range(len(dataset)):
-                if dataset[i][j] == 0 or dataset[i][j]==-1:
-                    dataset[i][j] = math.inf
-                    
-                    
+    def Algo(self):
+        starttime = time.time()
+        dataset = self.dataset    
         l = len(dataset)
-        nodes = combine(dataset, l)
-        parameters = antcolony(self.n, self.iteration, self.a, self.b, self.r, self.q, self.s)
-        dist, OptimalTour = parameters.ant(nodes)
+        """
+        We'll use inf to denote unconnected cities. Since, in TSPLIB (which we used for benchmarking had different 
+        values denoting unconnectivity - 9999999, 0, 100000000, -1), we'll replace all these values with inf. These values 
+        have to be modified if new value for denoting unonnected cities is encountered. These values work fine for the 
+        TSPLIB problems we tested.
+        """
+        for i in list(range(0,l)):
+            for j in list(range(0,l)):
+                if dataset[i][j]==0 or dataset[i][j]==-1 or dataset[i][j]==9999999 or dataset[i][j]==100000000:
+                    dataset[i][j] = float('inf')
+                    
+        antcolonyobject = antcolony(self.ants, self.iterations, alpha=1.0, beta=10.0, rho=0.5, q=10)
+        """
+        alpha controls influence of pheromones in a tansition
+        beta controls the desirability of a transition
+        rho is evaporation rate, controls how pheromones vanish. It lies in [0,1]
+        q is a constant.
+        """
+        combine = Combine(dataset, l) # To present length and cost matrix
+        OptimalTour, mindist = antcolonyobject.solve(combine)
         OptimalTour = [x+1 for x in OptimalTour]
         
         OptimalTour = list(OptimalTour)
@@ -40,108 +53,93 @@ class AntApproach:
             OptimalTour = List2 + List1
         
         OptimalTour.append(1)
-
-        end_time = time.time() # End clock
-        return dist, OptimalTour, (end_time-start_time)
+        return mindist, OptimalTour, (time.time()-starttime)
         
-class combine(object):
-    def __init__(self,dataset,l):
-        self.dataset = dataset
-        self.l = l # Number of nodes
-        self.pheromone = [[1/(l*l) for j in range(l)] for i in range(l)]
-  
+class Combine(object):
+    def __init__(self, cost_matrix: list, size: int):
+        self.matrix = cost_matrix
+        self.size = size
+        self.pheromone = [[1 / (size * size) for j in range(size)] for i in range(size)] # Pheromone, will be updated et every iteration
+
+
 class antcolony(object):
-    def __init__(self,n,iteration,a,b,r,q,s):
-        self.n = n # number of ants
-        self.iteration = iteration
-        self.a = a # parameter alpha; See definition of parameters at https://en.wikipedia.org/wiki/Ant_colony_optimization_algorithms#Convergence
-        self.b = b # Parameter beta
-        self.r = r # pheromone evaporation coefficient 
-        self.q = q # it's a constant
-        self.s = s # Update strategy
-    
-    def pheromoneupdate(self, nodes, run):
-        for i, row in enumerate(nodes.pheromone):
+    def __init__(self, ant_count: int, iterations: int, alpha: float, beta: float, rho: float, q: int):
+        self.Q = q
+        self.rho = rho
+        self.beta = beta
+        self.alpha = alpha
+        self.ant_count = ant_count
+        self.iterations = iterations
+        
+    def pheromoneupdate(self, combine: Combine, ants: list):
+        for i, row in enumerate(combine.pheromone):
             for j, col in enumerate(row):
-                nodes.pheromone[i][j] = nodes.pheromone[i][j]*self.r
-                for ant in run:
-                    nodes.pheromone[i][j] = nodes.pheromone[i][j] + ant.updateself[i][j]
+                combine.pheromone[i][j] *= self.rho
+                for ant in ants:
+                    combine.pheromone[i][j] += ant.pheromone_delta[i][j]
 
-    
-    def ant(self, nodes):
-        # Set optimal tour to be infinite 
-        mindist = math.inf
-        # Create an empty list for optimal tour
-        OptimalTour = []
-        for iteration in range(self.iteration):
-            run = [selection(self, nodes) for i in range(self.n)]
-            for ant in run:
-                for i in range(nodes.l-1):
+
+    def solve(self, combine: Combine):
+        best_cost = float('inf')
+        best_solution = []
+        for gen in range(self.iterations):
+            ants = [_Ant(self, combine) for i in range(self.ant_count)]
+            for ant in ants:
+                for i in range(combine.size - 1):
                     ant.nextvisit()
-                ant.distance = ant.distance + nodes.dataset[ant.tour[-1]][ant.tour[0]]
-                if ant.distance < mindist:
-                    mindist = ant.distance
-                    OptimalTour = [] + ant.tour
-                ant.updateit() # Local update of pheromone
-            self.pheromoneupdate(nodes, run)
-        return mindist, OptimalTour
-                               
-class selection(object):
-    def __init__(self, parameters, nodes):
-        self.colony = parameters
-        self.nodes = nodes
-        self.distance=0
-        self.tour = [] # Tour yet
-        self.update = [] # Local update of pheromone
-        self.next = [i for i in range(nodes.l)] # next possible visits
-        # probability to visit a node
-        self.prob = [[0 if i==j else 1/float(nodes.dataset[i][j]) for j in range(nodes.l)] for i in range(nodes.l)]
-        # Select first node between 0 and (l-1) randomly
-        random.seed(1)
-        first = random.randint(0,(nodes.l-1))
-        # first = 0 # It did not work by changing first city to 0. I shall later update this.
-        self.tour.append(first) # Add start to the tour
-        self.current = first # Current node
-        self.next.remove(first) # Remove start from the next possible visits
-               
+                ant.total_cost += combine.matrix[ant.tour[-1]][ant.tour[0]]
+                if ant.total_cost < best_cost:
+                    best_cost = ant.total_cost
+                    best_solution = [] + ant.tour
+                # update pheromone
+                ant.pheromoneupdate_delta()
+            self.pheromoneupdate(combine, ants)
+        return best_solution, best_cost
+
+
+class _Ant(object):
+    def __init__(self, antcolonyobject: antcolony, combine: Combine):
+        self.colony = antcolonyobject
+        self.combine = combine
+        self.total_cost = 0.0
+        self.tour = []  # tour list
+        self.pheromone_delta = []  # the local increase of pheromone
+        self.allowed = [i for i in range(combine.size)]  # cities which are allowed for the next visit
+        self.eta = [[0 if i == j else 1 / combine.matrix[i][j] for j in range(combine.size)] for i in
+                    range(combine.size)]  # Desirability
+        start = random.randint(0, combine.size - 1)  # start from any city
+        self.tour.append(start)
+        self.current = start
+        self.allowed.remove(start)
+
     def nextvisit(self):
-        p = [0 for i in range(self.nodes.l)]  # probabilities for moving to a node in the next step
-        sum = 0
-        for i in self.next:
-            sum = sum + self.nodes.pheromone[self.current][i]**self.colony.a * self.prob[self.current][i]**self.colony.b
-        for i in range(self.nodes.l):
-            if i in self.next:
-                if self.nodes.dataset[self.current][i] != math.inf:
-                    try:
-                        p[i] = (self.nodes.pheromone[self.current][i]**self.colony.a * self.prob[self.current][i]**self.colony.b) /(sum)               
-                    except ZeroDivisionError:
-                        pass            # To avoid division by zero since sum is originally 0
-        # select next node
-        select = 0
-        randomvalue = random.random() # Select a random value between 0 and 1
-        for i, d in enumerate(p):
-            randomvalue = randomvalue - d
-            if randomvalue <= 0:
-                select = i
+        denominator = 0
+        for i in self.allowed:
+            denominator += self.combine.pheromone[self.current][i] ** self.colony.alpha * self.eta[self.current][
+                                                                                            i] ** self.colony.beta
+        probabilities = [0 for i in range(self.combine.size)]  # probabilities for moving to a city in the next step
+        for i in range(self.combine.size):
+            try:
+                self.allowed.index(i)  # test if allowed list contains i
+                probabilities[i] = (self.combine.pheromone[self.current][i] ** self.colony.alpha) * (self.eta[self.current][i] ** self.colony.beta) / denominator
+            except ValueError or ZeroDivisionError:
+                pass  
+        # select next city randomly with probability - list probabilities
+        selected = 0
+        rand = random.random()
+        for i, probability in enumerate(probabilities):
+            rand -= probability
+            if rand <= 0:
+                selected = i
                 break
-        
-        try:
-            self.next.remove(select) # To avoid ValueError
-        except ValueError:
-            pass
-            
-        self.tour.append(select) 
-        self.distance = self.distance + self.nodes.dataset[self.current][select]
-        self.current = select
-        
-    def updateit(self):
-        self.updateself = [[0 for j in range(self.nodes.l)] for i in range(self.nodes.l)]
-        for k in range(1, len(self.tour)):
-            i = self.tour[k - 1]
-            j = self.tour[k]
-            if self.colony.s == 1:
-                self.updateself[i][j] = self.colony.q
-            elif self.colony.s == 2:
-                self.updateself[i][j] = self.colony.q / self.nodes.dataset[i][j]
-            else:  # ant-cycle system
-                self.updateself[i][j] = self.colony.q / self.distance
+        self.allowed.remove(selected)
+        self.tour.append(selected)
+        self.total_cost += self.combine.matrix[self.current][selected]
+        self.current = selected
+
+    def pheromoneupdate_delta(self):
+        self.pheromone_delta = [[0 for j in range(self.combine.size)] for i in range(self.combine.size)]
+        for _ in range(1, len(self.tour)):
+            i = self.tour[_ - 1]
+            j = self.tour[_]
+            self.pheromone_delta[i][j] = self.colony.Q / self.total_cost
